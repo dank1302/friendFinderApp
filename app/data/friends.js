ff463e757c1790a760362e4ddcb090b33a71b3a8
DK1302 <dank1302@yahoo.com>
2018-08-10 22:20:52 -0400

    friendApp

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// Variable to hold the array of friends objects data
+var friends = [{ "name": "Cassandra", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, 
+{ "name": "Mark", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, 
+{ "name": "Julie", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+{ "name": "Louis", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+{ "name": "Ashley", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, 
+{ "name": "Jordan", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
